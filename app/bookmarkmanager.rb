524de429df7d727607950e0c15e26d43d7001d5c
xiaoting wright <haley.xt.wright@gmail.com>
2013-10-01 18:38:06 +0100

    add singing up function

--- app/bookmarkmanager.rb
+++ app/bookmarkmanager.rb
@@ -31,6 +31,39 @@ get '/tags/:text' do
 end
 
 
+get '/users/new' do
+  # note the view is in views/users/new.erb
+  # we need the quotes because otherwise
+  # ruby would divide the symbol :users by the
+  # variable new (which makes no sense)
+  erb :"users/new"
+end
+
+
+
+
+enable :sessions
+set :session_secret, 'super secret'
+
+
+
+post '/users' do
+  user = User.create(:email => params[:email], 
+                     :password => params[:password])  
+  session[:user_id] = user.id
+  redirect to('/')
+end
+
+helpers do
+
+  def current_user    
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  end
+
+end
+
+
+
 env = ENV["RACK_ENV"] || "development"
 # step one: create a conection with the database I have created
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
